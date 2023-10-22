49d85b22fcd70acc1fa5a54d110c4c90fc95dc73
gajulapallihitesh <gajulapallihitesh7@gmail.com>
2023-10-22 13:19:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import './App.css';
+import Details from './components/Details';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+   <Details/>
+      
+    </div>
+  );
+}
+
+export default App;
